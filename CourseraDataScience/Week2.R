d08dd6f433cc5b96e0cb6b7b85351e264a4449f6
d25higgins <d25higgins@gmail.com>
2014-12-06 14:10:39 -0800

    More week 2 work

--- CourseraDataScience/Week2.R
+++ CourseraDataScience/Week2.R
@@ -1,2 +1,23 @@
-#Week 2
+#Week 2 Quizz
+
+#Question 3
+f <- function(x){
+    g <- function(y){
+        y + z
+    }
+    z <- 4
+    x + g(x)
+}
+z <- 10
+f(3)
+
+#Question 4
+x <- 5
+y <- if(x<3){
+    NA
+}else{
+    10
+}
+
+y
 
